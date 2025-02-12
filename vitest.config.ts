ab0d7c0c8b34e37703e20775ba0eb0e5c55d81b2
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-02-12 18:49:45 +0000

    feat - add basic features and improvements to build process

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   test: {
     globals: true,
     coverage: {
-        provider: 'v8',
+      provider: "v8",
     },
-    environment: 'jsdom',
-    setupFiles: './tests/setup.ts',
-    include: ['tests/**/*.test.ts', 'tests/**/*.test.tsx'], // Ensure tests are found
+    environment: "jsdom",
+    setupFiles: "./tests/setup.ts",
+    include: ["tests/**/*.test.ts", "tests/**/*.test.tsx"], // Ensure tests are found
   },
 });
