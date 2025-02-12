caf4a78902e67bf32d71789574e32bda152e7a12
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-02-12 14:25:22 +0000

    feat - add basic features including diagram and auth fetch functionality

---
+++ src/Diagram.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import MermaidDiagram  from "./MermaidDiagram,";
+
+const Diagram = ({ events }: { events: any[] }) => {
+  const [definition, setDefinition] = useState("");
+
+  useEffect(() => {
+    const diagramDef = `
+      graph TD
+      ${events
+        .map(
+          (event, index) =>
+            `${event.type}_${index}([${event.type} - ${event.action}])`
+        )
+        .join("\n")}
+    `;
+    setDefinition(diagramDef);
+  }, [events]);
+
+  console.log(definition);
+
+  return (
+    <div className="diagram-container">
+      <MermaidDiagram definition={definition} />
+    </div>
+  );
+};
+
+export default Diagram;
