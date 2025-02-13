df3e1ef9cd235c3cd1aa0c197d52c1d62d77022e
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-02-13 10:19:50 +0000

    fix - Validate property value in MermaidDiagram component logic

--- src/components/MermaidDiagram,.tsx
+++ src/components/MermaidDiagram,.tsx
@@ -12,7 +12,7 @@ const MermaidDiagram: React.FC<MermaidDiagramProps> = ({ definition }) => {
     if (chartRef.current) {
       try {
         mermaid.initialize({ startOnLoad: false });
-        mermaid.render("mermaid-diagram", definition).then(({svg}) => chartRef.current.innerHTML = svg);
+        mermaid.render("mermaid-diagram", definition).then(({ svg }) => { if (chartRef.current === null) { return; } chartRef.current.innerHTML = svg });
       } catch (error) {
         console.error("Error rendering Mermaid diagram:", error);
       }
